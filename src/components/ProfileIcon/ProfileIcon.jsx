029dd3e00998d3ac4b681ffc20d4e8fd8eaf5a90
Scott <sithuaung2002@gmail.com>
2024-01-23 23:27:17 +0800

    feat: added userinfo and conditional link rendering for navbar

--- src/components/ProfileIcon/ProfileIcon.jsx
+++ src/components/ProfileIcon/ProfileIcon.jsx
@@ -2,19 +2,27 @@ import React, { useState, useEffect, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 import DefaultProfileImg from "../../assets/images/DefaultProfileImg.jpg";
-import "./ProfileIcon.scss"; 
+import "./ProfileIcon.scss";
 
 const ProfileIcon = ({ selectedItem, onItemClick }) => {
-  
   const isMobile = window.innerWidth <= 600;
   const [popoverVisible, setPopoverVisible] = useState(isMobile);
-  // ... other states and refs ...
   const popoverRef = useRef(null);
   const iconRef = useRef(null);
-  const email = localStorage.getItem("Email");
-  const userName = localStorage.getItem("UserName");
+  const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+  let email;
+  let userName;
+
+  if (userInfo) {
+    email = userInfo.email;
+    userName = userInfo.username;
+  }
+  const userLoggedIn = !!userInfo;
 
   useEffect(() => {
+    if (!localStorage.getItem("userInfo")) {
+      setPopoverVisible(false);
+  }
     const handleClickOutside = (event) => {
       if (
         popoverRef.current &&
@@ -38,34 +46,59 @@ const ProfileIcon = ({ selectedItem, onItemClick }) => {
   };
 
   const handleSettingsClick = () => {
-
-    onItemClick("Account"); 
+    onItemClick("Account");
     setPopoverVisible(false);
   };
 
+  const handleLogout = (event) => {
+    event.stopPropagation(); // Prevents event from propagating to parent elements
+    localStorage.removeItem("userInfo");
+    setTimeout(() => {
+      window.location.href = "/";
+    }, 1500);
+  };
+
+  const handleLoginClick = () => {
+    window.location.href = '/login';
+  };
+
+  const handleRegisterClick = () => {
+    window.location.href = '/register';
+  };
+
   return (
-
-        <div className="profileIcon" ref={iconRef} onClick={togglePopover}>
-          <FontAwesomeIcon icon={faUserCircle} />
-          {popoverVisible && (
-            <div className="profilePopover" ref={popoverRef}>
-              <div className="profileInfoContainer">
-                <img src={DefaultProfileImg} alt="" srcset="" />
-                <div>
-                  <p>{userName}</p>
-                  <p>{email}</p>
-                </div>
-              </div>
-
-              <hr />
-              <div className="profilePopoverLinks">
-                <button onClick={handleSettingsClick}>Settings</button>
-                <button>Logout</button>
+    <div className="profileIcon" ref={iconRef} onClick={togglePopover}>
+      <FontAwesomeIcon icon={faUserCircle} />
+      {popoverVisible && (
+        <div className="profilePopover" ref={popoverRef}>
+          {userInfo && ( // Render this block only if userInfo is available
+            <div className="profileInfoContainer">
+              <img src={DefaultProfileImg} alt="" srcset="" />
+              <div>
+                <p>{userName}</p>
+                <p>{email}</p>
               </div>
             </div>
           )}
-        </div>
+          {userLoggedIn && <hr />}
 
+
+          <div className="profilePopoverLinks">
+          {userLoggedIn ? (
+              <>
+                <button onClick={handleSettingsClick}>Settings</button>
+                <button onClick={handleLogout}>Logout</button>
+              </>
+            ) : (
+              <>
+                <button onClick={handleLoginClick}>Login</button>
+                <button onClick={handleRegisterClick}>Register</button>
+              </>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
