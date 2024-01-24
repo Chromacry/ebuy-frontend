5285d5f26133865340d389870d31bfb6cf373ccb
Chromacry <Chromacry101@gmail.com>
2024-01-24 18:18:30 +0800

    feat: added global constants and imageencodingutil

---
+++ src/constants/GlobalConstants.js
@@ -0,0 +1,8 @@
+export const STATUS_CODES = {
+  SUCCESS_CODE : 200,
+  BAD_REQUEST_CODE : 400,
+  UNAUTHORIZED_CODE : 401,
+  INTERNAL_SERVER_ERROR_CODE : 500
+}
+export const rowsPerPage = 10;
+export const toastDuration = 10000; //* milliseconds
