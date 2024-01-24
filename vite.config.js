eb68e3747e19e09fd6213689ecf77655275aeddd
Chromacry <Chromacry101@gmail.com>
2024-01-24 18:17:43 +0800

    feat: added datetimeutil; added moment; updated vite config;

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+
+    globals: true,
+    environment: 'jsdom',
+    reporters: 'verbose',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
