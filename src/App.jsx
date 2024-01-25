ed396f1728cdba9fe283a40bb4741558fc38933c
Scott <sithuaung2002@gmail.com>
2024-01-26 03:35:17 +0800

    feat: added protected routes for only authorized sellers bug: fixed the bug of 2 users logged into the same account from different pc causing errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React, { createContext, useState, useEffect } from "react";
 import "./App.scss";
+import ProtectedRoute from "./ProtectedRoute";
 import Home from "./pages/Home/Home";
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/login";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Admin from "./pages/Admin/Admin";
 import { getUserApi } from "./services/APIs/UserApi";
 import { getUserInfoFromLocalStorage, setUserInfoInLocalStorage } from "./utils/LocalStorageUntil";
@@ -19,22 +20,46 @@ const App = () => {
     setTheme(newTheme);
     localStorage.setItem("theme", newTheme);
   };
-  const getUser = async (e) => {
+  const getUser = async () => {
     const response = await getUserApi();
-    let accessToken = getUserInfoFromLocalStorage().accessToken
-    setUserInfoInLocalStorage({
-      accessToken : accessToken,
-      username: response.username,
-      email: response.email,
-      id: response.id,
-      created_time: response.created_time,
-      profile_image: response.profile_image
 
-    })
+    console.log(response.message)
+    if (response.message === "Authenticated") {
+        let accessToken = getUserInfoFromLocalStorage().accessToken;
+        setUserInfoInLocalStorage({
+            accessToken : accessToken,
+            username: response.username,
+            email: response.email,
+            id: response.id,
+            created_time: response.created_time,
+            profile_image: response.profile_image,
+            is_seller: response.is_seller
+        });
+    } else {
+      console.log("logout initiated status not equal to 200")
+      localStorage.removeItem("userInfo");
+      setTimeout(() => {
+        window.location.href = "/";
+      }, 1500);
+    }
 };
+
 if(localStorage.getItem("userInfo")){
   getUser();
 }
+
+
+
+
+const RouteChangeListener = () => {
+  const location = useLocation();
+  useEffect(() => {
+    if (localStorage.getItem("userInfo")) {
+      getUser();
+    }
+  }, [location]);
+  return null; 
+};
 
 
   return (
@@ -42,12 +67,17 @@ if(localStorage.getItem("userInfo")){
       <ThemeContext.Provider value={{ theme, toggleTheme }}>
         <div className="Theme" id={theme}>
           <BrowserRouter>
+          <RouteChangeListener/>
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/product-details/:productId" element={<ProductDetails />} />
               <Route path="/register" element={<Register />} />
               <Route path="/login" element={<Login />} />
-              <Route path="/admin" element={<Admin />} />
+              <Route path="/admin" element={
+                <ProtectedRoute>
+                  <Admin />
+                </ProtectedRoute>
+              } />
             </Routes>
           </BrowserRouter>
         </div>
