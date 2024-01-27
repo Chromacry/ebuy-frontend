3b53210192bad4d2c257d67ed8201e224b45a55d
ILikeToEatSeaweed <yaoyt1995@gmail.com>
2024-01-27 21:50:43 +0800

    feat: Cart feature implemented

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -2,14 +2,17 @@ import "./ProductDetails.scss";
 import React, { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
-import productImage from '../../assets/images/heel.png';
+import productImage from "../../assets/images/heel.png";
 import { getProductApi } from "../../services/APIs/ProductApi";
-import { addItemToCart, updateItemQuantityFromCart } from "../../utils/CartUtil";
+import {
+  addItemToCart,
+  updateItemQuantityFromCart,
+} from "../../utils/CartUtil";
 const ProductDetails = () => {
   const { productId } = useParams();
-  const indexOfLastI = productId.lastIndexOf('i');
+  const indexOfLastI = productId.lastIndexOf("i");
   const itemId = productId.substring(indexOfLastI + 1);
-  
+
   const [productQuantity, setProductQuantity] = useState(1);
   const [productDetails, setProductDetails] = useState(null);
   const [successMessage, setSuccessMessage] = useState("");
@@ -29,33 +32,36 @@ const ProductDetails = () => {
       console.error("Error retrieving product:", error);
       setErrorMessage("An error occurred while retrieving the product.");
     }
-  }
-  
+  };
+
   const addProductToCart = async () => {
     const body = {
       product_id: itemId,
       order_quantity: productQuantity,
       product_price: 40,
-    }
+    };
     addItemToCart(body);
-  }
+  };
   useEffect(() => {
     getProduct();
-  }, [])
+  }, []);
   return (
     <div className="body">
       <Navbar />
       <div className="product-wrapper">
         <div className="imageContainer">
-          <img src={productDetails?.product_image} alt={productDetails?.product_name} />
+          <img
+            src={productDetails?.product_image}
+            alt={productDetails?.product_name}
+          />
         </div>
         <div className="product-content">
           <div className="store-profile">
-          <img
-            src={productImage}
-            alt={"store profile"}
-            style={{ width: "50px", height: "50px" }}
-          />
+            <img
+              src={productImage}
+              alt={"store profile"}
+              style={{ width: "50px", height: "50px" }}
+            />
             <h4>Nike Store</h4>
           </div>
           <h2>{productDetails?.product_name}</h2>
@@ -71,15 +77,30 @@ const ProductDetails = () => {
           <div className="product-options">
             <h5>Quantity:</h5>
             <div className="quantity-button">
-              <button className="quantity-action" onClick={() => setProductQuantity((productQuantity) => productQuantity <= 1 ? 1 : productQuantity - 1)}>-</button>
+              <button
+                className="quantity-action"
+                onClick={() =>
+                  setProductQuantity((productQuantity) =>
+                    productQuantity <= 1 ? 1 : productQuantity - 1
+                  )
+                }
+              >
+                -
+              </button>
               <span className="quantity-display">{productQuantity}</span>
-              <button className="quantity-action" onClick={() => setProductQuantity((productQuantity) => productQuantity + 1)}>+</button>
+              <button
+                className="quantity-action"
+                onClick={() =>
+                  setProductQuantity((productQuantity) => productQuantity + 1)
+                }
+              >
+                +
+              </button>
             </div>
           </div>
           <div className="buttonsCallToAction">
-          <button onClick={addProductToCart}>Add To Cart</button>
-          <button className="buyNowButton">Buy Now</button>
-
+            <button onClick={addProductToCart}>Add To Cart</button>
+            <button className="buyNowButton">Buy Now</button>
           </div>
         </div>
       </div>
