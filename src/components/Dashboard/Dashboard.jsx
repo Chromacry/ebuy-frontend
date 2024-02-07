e9e2402bac84414f1b942b523811516f330efcd2
Scott <sithuaung2002@gmail.com>
2024-02-07 16:28:45 +0800

    TEST V4

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -24,22 +24,8 @@ const Dashboard = () => {
         <button onClick={DeleteAccount}>Delete Account</button>
       </div>
       <div className="GridItem">Test</div>
-      <div className="GridItem">TEST 3
-      </div>
-      <div className="GridItem">
-        Dashboard Component Content Lorem ipsum, dolor sit amet consectetur
-        adipisicing elit. Maiores modi iusto deleniti officiis, fuga beatae
-        adipisci porro eveniet culpa officia, sit minus incidunt voluptatibus
-        fugit doloribus facilis et recusandae quam. Lorem ipsum dolor sit amet
-        consectetur, adipisicing elit. Quod iste nam autem, perspiciatis
-        molestias cumque illo quam consequuntur dolore quis, necessitatibus
-        fugit, ad voluptatem deleniti rerum. Iure sunt facere pariatur? Lorem
-        ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis similique
-        commodi illo placeat magnam, quod debitis alias numquam praesentium
-        dicta eaque expedita iste quis consequatur tempora veritatis ex saepe
-        assumenda. Loa, consequuntur ipsam sint sequi quasi. Repellat quae sed
-        molestias dolor cum possimus suscipit optio ducimus cumque quidem!Lorem
-      </div>
+      <div className="GridItem">TEST 3</div>
+      <div className="GridItem">TEST 4</div>
 
       <div className="GridItem">
         Dashboard Component Content Lorem ipsum, dolor sit amet consectetur
